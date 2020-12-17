010c6ba900da120d825baa6ce72cdf5c7bf53895
souveekp <souveek.rishi123@gmail.com>
2020-12-17 18:58:38 +0530

    Minor Updates

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,13 +5,8 @@ public class TicTacToe{
 
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
-				Player p1 = new Player();
-				Player p2 = new Player();
-				p1.setName("Player 1");
-				p2.setName("Player 2");
 				GameLogic gl = new GameLogic();
-				gl.initializeGame();
-				// gl.addPlayerDetails(p1.getName(), p2.getName());
+				gl.getPlayerDetails();
 			}
 		});
 		
