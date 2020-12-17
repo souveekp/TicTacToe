bd372a853d5ec67a5703d260d3f8df018afdff0f
souveekp <souveek.rishi123@gmail.com>
2020-12-17 18:54:06 +0530

    Added functionalities to Player

--- Player.java
+++ Player.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class Player{
 	String name;
-	int score;
+	int score = 0;
 	Scanner sc = new Scanner(System.in);
 	public String getName(){
 		return name;
 	}
-	public void setName(String playerNumber){
-		System.out.println("Enter "+playerNumber+" Name");
-		this.name = sc.nextLine();
+	public void setName(String playerName){
+		this.name = playerName;
 	}
 	public int getScore(){
 		return score;
